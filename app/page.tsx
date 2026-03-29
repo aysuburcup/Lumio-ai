b8d0d7c4f838d90a4c1e7dfc87c87cd569bbf9d1
Aysu Burcu Parlak <aysuburcu123@gmail.com>
2026-03-29 21:36:28 +0300

    fix homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main style={{ padding: 20 }}>
-      <h1>Lumio AI</h1>
-      <p>Proje çalışıyor 🚀</p>
+      <h1>Lumio AI 🚀</h1>
+      <p>Proje çalışıyor 🎉</p>
     </main>
   );
 }
